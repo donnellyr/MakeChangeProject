8d2ea0bdc9c0324c5bfc2131429f87b3bd802944
Ryan Donnelly <ryan@Ryans-MBP.home>
2022-06-18 17:14:18 -0600

    Added menue to allow for additional trasactions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MakeChangeProject
 ## Description
-A cash register app that will take user input and calculate how much currency needs to be tendered in correct denominations.
+A cash register app that will take user input and calculate how much currency needs to be tendered in correct denominations. Intended for use is java ver 1.8
 ## Technologies used
 Scanner - The scanner was used to receive the user input to be calculated.
 
@@ -11,6 +11,8 @@ Float - Casted double values from user input to floats to have values rounded up
 Int - Ints were used to store the amount of each individual currency and make the end product look cleaner when printed, since they will always be whole numbers as you can't have a fraction of a quarter or dime.
 
 If statement - used to execute certain functions when their corresponding denominations of money are used.
+
+Switch statement - used to create a menue to restart the program should the user wish to make another transaction, and close it if they do not.
 
 While loop - used to count how much of each bill and or coin to return by incrementing the amount tendered and subtracting from the remainder of change left each loop.
 ## What i learned
--- src/comm/skilldistillery/makechange/MakeChange.java
+++ src/comm/skilldistillery/makechange/MakeChange.java
@@ -5,27 +5,51 @@ import java.util.Scanner;
 public class MakeChange {
 
 	public static void main(String[] args) {
+		userInfo();
+		
+		
+		
+	}
+	public static void userInfo() {
+		String response;
 		Scanner input = new Scanner(System.in);
 		double pay = 0, price = 0;
+		
 		System.out.println("What is the price of the item?");
 		price = input.nextDouble();
-		System.out.println("How much did the customer pay?");
+		System.out.println("How much did the customer tender?");
 		pay = input.nextDouble();
 		String change = change(pay, price);
 		System.out.println(change);
-		input.close();
 		
+		System.out.println("Would you like to make another transaction?");
+		System.out.println("1. Yes");
+		System.out.println("2. No");
+		response = input.nextLine();
+		response = input.nextLine();
+		switch(response) {
+		case "1":
+		case "Yes":
+		case "yes":userInfo();
+		break;
+		case "2":
+		case "No":
+		case "no": System.out.println("Have a good day!");
+		break;
+		default: System.out.println("INVALID CHOICE");
+		}
+		input.close();
 		
 	}
 	public static String change(double pay, double price) {
 		float change = (float)(pay - price);
-		String output ="Item price: $" + price +" Amount payed: $" + pay + " ERROR INSUFICIANT FUNDS $" + (change * -1) + " NEEDED TO COMPLETE TRANSACTION";
+		String output ="Item price: $" + price +" Amount tendered: $" + pay + " ERROR INSUFICIANT FUNDS $" + (change * -1) + " NEEDED TO COMPLETE TRANSACTION";
 		int twenty = 0, ten = 0, five = 0, one = 0, quarter = 0, dime = 0, nickle = 0, penny = 0; 
 		if(change == 0) {
-			output = "Item price: $" + price +" Amount payed: $" + pay + " Exact amount recived no change needed";
+			output = "Item price: $" + price +" Amount tendered: $" + pay + " Exact amount recived no change needed";
 		}
 		else if(change > 0) {
-			output = "Item price: $" + price +" Amount payed: $" + pay + " Change owed: $" + change + " Tendered:";
+			output = "Item price: $" + price +" Amount tendered: $" + pay + " Change owed: $" + change + " Denominations distributed:";
 			while(change >= 20.0) {
 				change-=20.0;
 				twenty++;
